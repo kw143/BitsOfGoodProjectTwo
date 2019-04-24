52be21cd9c26b75f192eb642341a6118091c90f7
kw143 <kw142143@gmail.com>
2019-04-24 11:30:40 -0400

    settingroutes

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const todosRouter = require('./routes/todos');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,6 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
+app.use('/todos', todosRouter);
 app.use('/users', usersRouter);
 
 // Render React page
