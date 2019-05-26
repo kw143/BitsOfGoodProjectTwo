66d9e76f7bb784d82a813cbc8bb4a01a38876878
kw143 <kw142143@gmail.com>
2019-05-25 23:43:30 -0400

    remove build from gitignore

---
+++ client/build/precache-manifest.bbf913cbe3ec000412df5a6e73d098c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a463ce460dfba27fe969d602b0176b14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a08b29ca1121e7e76d7",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "178e624f66296adbefe7",
+    "url": "/static/js/2.46cf3131.chunk.js"
+  },
+  {
+    "revision": "0a08b29ca1121e7e76d7",
+    "url": "/static/js/main.cd111895.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
